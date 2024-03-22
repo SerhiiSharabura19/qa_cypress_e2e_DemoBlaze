d0bf1fecbd3f55b4e6d8b7be5c0789f2472031fc
Serhii Sharabura <youserhii@gmail.com>
2024-03-22 09:44:20 +0200

    morningTask

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,19 @@
 const { defineConfig } = require('cypress');
+const { faker } = require('@faker-js/faker');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://www.demoblaze.com/',
     setupNodeEvents(on, config) {
-    }
-  }
+      on('task', {
+        generateUser() {
+          return {
+            username: faker.internet.userName(),
+            email: faker.internet.email(),
+            password: 'Aa12345!',
+          };
+        },
+      });
+    },
+  },
 });
--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,11 +1,26 @@
 /// <reference types='cypress' />
-
-describe('Student Registration page', () => {
+describe('Demo Blaze Store', () => {
   before(() => {
-
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+    });
+    cy.visit('/');
   });
 
-  it('', () => {
+  it('should register a new user', () => {
+    cy.get('#signin2').click();
+    cy.get('#sign-username').type(user.username);
+    cy.get('#sign-password').type(user.password, `{enter}`);
+  });
 
+  it('should login the user', () => {
+    cy.get('#login2').click();
+    cy.get('#loginusername').type(user.username);
+    cy.get('#loginpassword').type(user.password, `{enter}`);
+  });
+
+  it('should login the user', () => {
+    cy.contains('[href="prod.html?idp_=1"]', 'Samsung galaxy s6').click();
+    cy.get('[onclick="addToCart(1)"]').click();
   });
 });
